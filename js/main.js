f2d045d463906a5a1c8e0ab541571fa51950dc6b
andreama0703 <54515549+andreama0703@users.noreply.github.com>
2024-01-23 20:38:19 +0100

    Commit sin JS

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let digito = document.getElementById("digito")
+
+digito 
+let resultado = 0
+
+btnsumar.addEventListener("click", function(){
+    if(digito1 != null){
+        digito1 = digito.value
+    }
+}
+)
